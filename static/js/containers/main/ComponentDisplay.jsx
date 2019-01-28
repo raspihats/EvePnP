e8eceaa07fd5c959f8ff4c9bc9e233222fce9cfe
Florin Costa <florin.costa83@gmail.com>
2019-01-28 21:38:21 +0200

    frontend, jobs edit, work in progress

--- static/js/containers/main/ComponentDisplay.jsx
+++ static/js/containers/main/ComponentDisplay.jsx
@@ -1,26 +1,19 @@
 import React from "react";
 
 const ComponentDisplay = props => {
-  let text = [];
-  text.push("value:" + props.value);
-  text.push("package:" + props.package);
-
   if (props.inline) {
-    let displayText = "";
-    text.map((element, index) => {
-      if (index > 0) {
-        displayText += ", ";
-      }
-      displayText += element;
+    let text = "";
+    Object.keys(props).forEach((key, index) => {
+      text += (index > 0 ? ", " : "") + key + ":" + props[key];
     });
-    return <p className="m-0 text-monospace">{displayText}</p>;
+    return <p className="m-0 text-monospace">{text}</p>;
   } else {
     return (
       <React.Fragment>
-        {text.map((element, index) => {
+        {Object.keys(props).map((key, index) => {
           return (
             <p key={index} className="m-0 text-monospace">
-              {element}
+              {key + ": " + props[key]}
             </p>
           );
         })}
--- static/js/containers/main/job/JobDetails.jsx
+++ static/js/containers/main/job/JobDetails.jsx
@@ -1,41 +1,99 @@
 import React from "react";
-
-const Row = props => {
-  return (
-    <tr className="text-muted">
-      <th scope="row">{props.data.id}</th>
-      <td>{props.data.value}</td>
-      <td>{props.data.package}</td>
-      <td>{props.data.x}</td>
-      <td>{props.data.y}</td>
-      <td>{props.data.angle}</td>
-      <td>{props.data.type}</td>
-      <td>{props.data.type}</td>
-    </tr>
-  );
-};
+import ComponentDisplay from "../ComponentDisplay";
+import PointDisplay from "../PointDisplay";
 
 const JobDetails = props => {
   return (
-    <table id="jobDetails" className="table table-hover table-sm border">
-      <thead className="text-secondary">
-        <tr>
-          <th scope="col">ID</th>
-          <th scope="col">Value</th>
-          <th scope="col">Package</th>
-          <th scope="col">X</th>
-          <th scope="col">Y</th>
-          <th scope="col">θ</th>
-          <th scope="col">Type</th>
-          <th scope="col">Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.job.components.map((element, index) => {
-          return <Row data={element} key={element.name + "_" + index} />;
-        })}
-      </tbody>
-    </table>
+    <React.Fragment>
+      <div className="row border bg-light py-2 text-info font-weight-bold">
+        <div className="col-2">ID</div>
+        <div className="col-3">Component</div>
+        <div className="col-2">Point</div>
+        <div className="col-1">Rotation</div>
+        <div className="col-2">Operation</div>
+        <div className="col-2">Action</div>
+      </div>
+      {props.job.components.map((component, index) => {
+        let cmp = { value: component.value };
+        component.package && (cmp.package = component.package);
+        component.footprint && (cmp.footprint = component.footprint);
+        return (
+          <div key={component.id} className="row text-info border border-top-0">
+            <div className="col-2">{component.id}</div>
+            <div className="col-3">
+              <ComponentDisplay {...cmp} />
+            </div>
+            <div className="col-2">
+              <PointDisplay {...component} />
+            </div>
+            <div className="col-1">{component.angle}</div>
+            <div className="col-2">{component.operation}</div>
+            {/* <div className="col-2">
+              <div className="row">
+                <div className="col-12">
+                  <OffsetButtons
+                    title="Move"
+                    onClick={offset => {
+                      let positions = {};
+                      Object.keys(feeder.point).map(key => {
+                        positions[key] = feeder.point[key];
+                        if (offset.hasOwnProperty(key)) {
+                          positions[key] += offset[key];
+                        }
+                      });
+                      api.axis.positions.update(positions);
+                    }}
+                  />
+                </div>
+                <div className="col-12">
+                  <button
+                    className="btn btn-block btn-outline-secondary btn-lg px-1 py-0"
+                    type="button"
+                    title="Show edit panel"
+                    data-toggle="collapse"
+                    data-target={"#" + feeder.id}
+                    aria-expanded="false"
+                    aria-controls={feeder.id}
+                    onClick={e => console.log(e)}
+                  >
+                    <FontAwesomeIcon icon="angle-double-down" />
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="col-12">
+              <div className="collapse" id={feeder.id}>
+                <UpdateForm
+                  feeder={feeder}
+                  onUpdate={updatedFeeder =>
+                    this.updateFeeder(feeder.id, updatedFeeder)
+                  }
+                />
+              </div>
+            </div> */}
+          </div>
+        );
+      })}
+    </React.Fragment>
+    // <table id="jobDetails" className="table table-hover table-sm border">
+    //   <thead className="text-secondary">
+    //     <tr>
+    //       <th scope="col">ID</th>
+    //       <th scope="col">Value</th>
+    //       <th scope="col">Package</th>
+    //       <th scope="col">X</th>
+    //       <th scope="col">Y</th>
+    //       <th scope="col">θ</th>
+    //       <th scope="col">Type</th>
+    //       <th scope="col">Action</th>
+    //     </tr>
+    //   </thead>
+    //   <tbody>
+    //     {props.job.components.map((element, index) => {
+    //       return <Row data={element} key={element.name + "_" + index} />;
+    //     })}
+    //   </tbody>
+    // </table>
   );
 };
 
